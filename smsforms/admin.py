549aa82901c40d44096a1b4794b0afe2d236d080
Cory Zue <czue@dimagi.com>
2012-08-23 16:25:45 +0200

    tweak admin names

--- smsforms/admin.py
+++ smsforms/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.contrib import messages
 from models import XFormsSession, DecisionTrigger
 
-class SessionAdmin(admin.ModelAdmin):
+class XFormsSessionAdmin(admin.ModelAdmin):
     list_display = ('session_id', 'connection','start_time', 'end_time', 'ended')
-admin.site.register(XFormsSession, SessionAdmin)
+    list_filter = ('ended',)
+
+admin.site.register(XFormsSession, XFormsSessionAdmin)
 
 class DecisionTriggerAdmin(admin.ModelAdmin):
     list_display = ('xform', 'trigger_keyword')
